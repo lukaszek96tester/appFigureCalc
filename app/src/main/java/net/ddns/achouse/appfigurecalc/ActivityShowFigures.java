1ab786fefffdab2a69f149e4fa5f3102e23cf4d2
Czapla <czapla.adrian8080@gmail.com>
2018-11-09 17:15:03 +0100

    + dodano button do landscape

--- app/src/main/java/net/ddns/achouse/appfigurecalc/ActivityShowFigures.java
+++ app/src/main/java/net/ddns/achouse/appfigurecalc/ActivityShowFigures.java
@@ -5,14 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
